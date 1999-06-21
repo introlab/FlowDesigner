49b2baf71b9ebd9a680ee506a3f6a4e31ded9769
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
1999-06-21 05:03:19 +0000

    Previously NetworkException.h

---
+++ FlowDesigner/data-flow/include/Exception.h
@@ -0,0 +1,187 @@
+#ifndef _NETWORKEXCEPTION_H_
+#define _NETWORKEXCEPTION_H_
+
+//#include "Node.h"
+#include <iostream.h>
+#include <string>
+#include "rc_ptrs.h"
+
+/***************************************************************************/
+/*
+  BaseException
+  Dominic Letourneau
+ */
+/***************************************************************************/
+/** This is the base class of all exceptions in the network. You should 
+    always derive from this class if you want to make a new exception 
+    handler.
+    @author Dominic Letourneau
+    @version 1.0
+*/
+class BaseException {
+public:
+
+   /** This pure virtual function is intended for debug purposes.
+       Every exception should be able to print a message. */
+   virtual void print (ostream &out = cerr) = 0;
+
+   ///Destructor
+   virtual ~BaseException(){;}
+
+};
+
+typedef counted_ptr<BaseException> ExceptionRef;
+
+
+/***************************************************************************/
+/*
+  NodeNotFoundException
+  Dominic Letourneau
+ */
+/***************************************************************************/
+/** The NodeNotFoundException is thrown when a node isn't in the nodeDictionary
+    @author Dominic Letourneau
+    @version 1.0
+*/
+class NodeNotFoundException : public BaseException {
+
+public:
+
+   ///This constructor is used to specify which node is not found.
+   NodeNotFoundException(string name) {
+      nodeName = name;
+   }   
+   ///The print method that prints on stderr by default
+   virtual void print(ostream &out = cerr) {
+      out<<"NodeNotFoundException occured, nodeName: "<<nodeName<<endl;
+   }
+   ///The node name.
+   string nodeName;
+};
+
+/***************************************************************************/
+/*
+  NoSinkNodeException
+  Dominic Letourneau
+ */
+/***************************************************************************/
+/** NoSinkNodeException occurs when the network is not properly initialized.
+    @author Dominic Letourneau
+    @version 1.0
+*/
+class  NoSinkNodeException : public BaseException {
+
+public:
+   ///default constructor
+   NoSinkNodeException() {errorNo = 0;}
+
+   ///constructor with an error number
+   NoSinkNodeException(int value) {
+      errorNo = value;
+   }
+   ///The print method
+   virtual void print(ostream &out = cerr) {
+      out<<"NoSinkNodeException occured, errorNo: "<<errorNo<<endl;
+   }
+   ///the error number
+   int errorNo;
+};
+
+/***************************************************************************/
+/*
+  NoInputNodeException
+  Dominic Letourneau
+ */
+/***************************************************************************/
+/** NoInputNodeException occurs when the network has no input node defined.
+    @author Dominic Letourneau
+    @version 1.0
+*/
+class  NoInputNodeException : public BaseException {
+
+public:
+
+   ///default constructor
+   NoInputNodeException() {errorNo = 0;}
+
+   ///constructor with an error value
+   NoInputNodeException(int value) {
+      errorNo = value;
+   }
+
+   ///the print method
+   virtual void print(ostream &out = cerr) {
+      out<<"NoInputNodeException occured, errorNo: "<<errorNo<<endl;
+   }
+
+   ///the error number
+   int errorNo;
+};
+
+
+/***************************************************************************/
+/*
+  FactoryNotFoundException
+  Dominic Letourneau
+ */
+/***************************************************************************/
+/** The FactoryNotFoundException occurs when we try to create
+    a new node with an unknown factory.
+    @author Dominic Letourneau
+    @version 1.0
+*/
+class FactoryNotFoundException : public BaseException {
+
+public:
+
+   ///The constructor with a factory name.
+   FactoryNotFoundException(string name) {
+      factoryName = name;
+   }   
+   ///The print method
+   virtual void print(ostream &out = cerr) {
+      out<<"FactoryNotFoundException occured, factoryName: "<<factoryName<<endl;
+   }
+   ///The factory name
+   string factoryName;
+};
+
+/***************************************************************************/
+/*
+  GeneralException
+  Jean-Marc Valin
+ */
+/***************************************************************************/
+/** The GeneralException is intended to write a generic message to the user
+    with file where the exception occured and the line number.
+    user __FILE__ and __LINE__ for convenience.
+    @author Jean-Marc Valin
+    @version 1.0
+*/
+class GeneralException :public BaseException {
+public:
+
+   ///The constructor that takes the message the file name and the line number
+   GeneralException(string _message, string _file, int _line) 
+      : message(_message)
+      , file (_file)
+      , line (_line)
+   {}
+
+   ///The print method
+   virtual void print(ostream &out = cerr)
+   {
+      out << file << " line " << line << ": " << message << endl;
+   }
+
+protected:
+   ///the message
+   string message;
+
+   ///the file name
+   string file;
+   
+   ///the line number
+   int line;
+};
+#endif
