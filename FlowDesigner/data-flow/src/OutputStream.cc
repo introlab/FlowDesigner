cc9a0c8b25a8a01935ea889861e25761deba91bd
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
1999-07-08 03:25:45 +0000

    new

---
+++ FlowDesigner/data-flow/src/OutputStream.cc
@@ -0,0 +1,32 @@
+#include "OutputStream.h"
+#include "net_types.h"
+
+OutputStream::OutputStream(string nodeName, ParameterSet params) 
+   : Node(nodeName, params)
+{
+   outputID = addOutput("OUTPUT");
+   inputID = addInput("INPUT");
+}
+
+void OutputStream::specificInitialize()
+{
+   this->Node::specificInitialize();
+}
+
+ObjectRef OutputStream::getOutput(int output_id, int count)
+{
+   if (output_id==outputID)
+   {
+      if (count != processCount)
+      {
+         NodeInput input = inputs[inputID];
+         string fileName = dereference_cast<string> (input.node->getOutput(input.outputID,count));
+         openedFile = ObjectRef (new OFStream());
+         ofstream &tmp = dereference_cast<ofstream> (openedFile);
+         tmp.open(fileName.c_str());
+      }
+      return openedFile;
+   }
+   else 
+      throw NodeException (this, "OutputStream: Unknown output id", __FILE__, __LINE__);
+}
