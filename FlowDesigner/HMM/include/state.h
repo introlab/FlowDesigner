94fe916816e10092dca4c22786ea71d5b7cb45d2
kiecza <kiecza@aa42df38-3a4e-474a-a597-ad7e395f5a33>
1999-07-09 01:02:39 +0000

    Corrected #define to be set to STATE_H

--- FlowDesigner/HMM/include/state.h
+++ FlowDesigner/HMM/include/state.h
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this file.  If not, write to the Free Software Foundation,
 // 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.#ifndef STATE_H
-#ifndef MODEL_H
+#ifndef STATE_H
 #define STATE_H
 
 
