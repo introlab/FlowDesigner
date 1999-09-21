2ec2a985714e800a896ce8de5d9b03001860766e
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
1999-09-21 05:46:42 +0000

    oops... spelling...

---
+++ FlowDesigner/HMM/include/acoustic_model.h
@@ -0,0 +1,61 @@
+// Copyright (C) 1999 Jean-Marc Valin
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this file.  If not, write to the Free Software Foundation,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+#ifndef ACCOUSTIC_MODEL_H
+#define ACCOUSTIC_MODEL_H
+
+#include "gmm.h"
+#include "gaussian.h"
+#include "covariance.h"
+#include "Object.h"
+#include <stream.h>
+#include <iostream.h>
+#include "covariance_set.h"
+#include "mean_set.h"
+#include "gaussian_set.h"
+#include "gmm_set.h"
+
+//#include "state.h"
+class AcousticModel : public Object{
+protected:
+   CovarianceSet covariances;
+   MeanSet means;
+   GaussianSet gaussians;
+   GMMSet gmms;
+   //vector<Ptr<GMM> > gmms;
+
+public:
+   AcousticModel() {}
+
+   /**convert all elements to using IDs (before saving)*/
+   void toIDs();
+ 
+   /**convert all elements to using Ptrs (after loading)*/
+   void toPtrs();
+
+   /** print function used for operator << */
+   virtual void printOn(ostream &out=cout) const;
+
+   /**Read function used for operator >> */
+   void readFrom (istream &in=cin);
+
+   /**extractor operator*/
+   friend istream &operator >> (istream &in, AcousticModel &mdl);
+   
+};
+
+#endif
---
+++ FlowDesigner/HMM/src/acoustic_model.cc
@@ -0,0 +1,88 @@
+// Copyright (C) 1999 Jean-Marc Valin
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this file.  If not, write to the Free Software Foundation,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#include "acoustic_model.h"
+#include "ObjectParser.h"
+#include "Object.h"
+
+DECLARE_TYPE(AcousticModel)
+
+void AcousticModel::toIDs()
+{
+   gmms.toIDs(gaussians);
+   gaussians.toIDs(means, covariances);
+}
+
+void AcousticModel::toPtrs()
+{
+   gaussians.toPtrs(means, covariances);
+   gmms.toPtrs(gaussians);
+   
+}
+
+void AcousticModel::printOn(ostream &out=cout) const
+{
+   out << "<AcousticModel " << endl;
+   out << "<covariances " << covariances << ">" << endl;
+   out << "<means " << means << ">" << endl;
+   out << "<gaussians " << gaussians << ">" << endl;
+   out << "<gmms " << gmms << ">" << endl;
+   //out << "<states " << states << ">" << endl;
+   //out << "<phones " << phones << ">" << endl;
+   out << ">\n";
+}
+
+void AcousticModel::readFrom (istream &in=cin)
+{
+   string tag;
+
+   while (1)
+   {
+      char ch;
+      in >> ch;
+      if (ch == '>') break;
+      else if (ch != '<') 
+       throw ParsingException ("Parse error: '<' expected");
+      in >> tag;
+      if (tag == "covariances")
+         in >> covariances;
+      else if (tag == "means")
+         in >> means;
+      else if (tag == "gaussians")
+         in >> gaussians;
+      else if (tag == "gmms")
+         in >> gmms;
+      //else if (tag == "states")
+      //   in >> states;
+      //else if (tag == "phones")
+      //   in >> phones;
+      else
+         throw ParsingException ("unknown argument: " + tag);
+
+      if (!in) throw ParsingException ("Parse error trying to build " + tag);
+
+      in >> tag;
+      if (tag != ">") 
+         throw ParsingException ("Parse error: '>' expected ");
+   }
+}
+
+istream &operator >> (istream &in, AcousticModel &mdl)
+{
+   if (!isValidType(in, "AcousticModel")) return in;
+   mdl.readFrom(in);
+   return in;
+}
