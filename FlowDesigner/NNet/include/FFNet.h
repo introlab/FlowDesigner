930f927c52e92d5c87b9d369b0b2a85dd4d6e105
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2000-02-19 00:02:50 +0000

    implementing Levenberg-Marquardt algorithm

--- FlowDesigner/NNet/include/FFNet.h
+++ FlowDesigner/NNet/include/FFNet.h
@@ -20,9 +20,12 @@ class FFNet : public Object {
    double *calc(const double *input);
 
    void learn(double *input, double *output, double alpha);
+   void learnlm(double *input, double *output, double **jacob, double *err, double &sse);
 
    void train(vector<float *> in, vector<float *> out, int iter, double learnRate=.00001, double mom=.9, 
 	      double increase=1.05, double decrease=.7, double errRatio=1.04);
+
+   void trainlm(vector<float *> in, vector<float *> out, int maxIter);
 
    void printOn(ostream &out) const;
    void readFrom (istream &in);
