6190f8842b3ddb4611b4f77358a3f4a20465e36d
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2000-05-19 06:17:25 +0000

    the best I can do for now...

--- FlowDesigner/audio_blocks/src/LTP.cc
+++ FlowDesigner/audio_blocks/src/LTP.cc
@@ -87,6 +87,16 @@ public:
       float best_gain=0;
       int best_T=0;
       float scores[end+1];
+      float gains[end+1];
+      /*static float old_scores[2000];
+      static bool init=false;
+      if (!init)
+      {
+	 for (int i=0;i<2000;i++)
+	    old_scores[i]=0;
+	 init=true;
+	 }*/
+
       for (int i=0;i<start;i++)
 	 scores[i]=0;
       for (int lag=start;lag<=end;lag++)
@@ -104,9 +114,10 @@ public:
 	       corr += in[i] * (*past)[inputLength+i-lag];
 	       energy += (*past)[inputLength+i-lag] * (*past)[inputLength+i-lag];
 	    } 
-	 //float score = corr/(energy+.000000001);
+	 float gain = corr/(energy+.000000001);
 	 float score = corr*corr/(energy+.000000001);
 	 scores[lag]=score;
+	 gains[lag]=gain;
 	 //cout << corr/energy << " ";
 	 if (score > best)
 	 {
@@ -119,39 +130,52 @@ public:
 	    } //else {cerr << "doubling\n";}
 	 }
       }
-
+/*
+      for (int i=start;i<=end;i++)
+      {
+	 scores[i] += .6*old_scores[i];
+	 old_scores[i]=scores[i];
+	 cout << scores[i] << " ";
+      }
+      cout << endl;
+*/
       for (int i=4;i>=2;i--)
       {
 	 int div = best_T/i;
 	 float max_score = 0;
+	 float max_gain = 0;
 	 int max_T=0;
 	 for (int j=div-2; j <= div+2 ; j++)
 	 {
 	    if (scores[j]>max_score)
 	    {
 	       max_score = scores[j];
+	       max_gain = gains[j];
 	       max_T = j;
 	    }
 	 }
-	 if (max_score*3 > best)
+	 if (max_score*2 > best || max_gain > .3)
 	 {
 	    if (i==2)
 	    {
 	       int div2 = best_T+max_T;
 	       float max_score2 = 0;
+	       float max_gain2 = 0;
 	       int max_T2=0;
 	       for (int j=div2-2; j <= div2+2 && j<end; j++)
 	       {
 		  if (scores[j]>max_score2)
 		  {
 		     max_score2 = scores[j];
+		     max_gain2 = gains[j];
 		     max_T2 = j;
 		  }		  
 	       }
-	       if (max_score2*3 < best)
+	       if (!(max_score2*2 > best || max_gain > .2))
 		  continue;
 	    }
 	    best_T = max_T;
+	    best_gain = max_gain;
 	    break;
 	 }
       }
