b91e106c1e7c43ac9244f9e5d965357435058317
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2000-05-30 21:36:53 +0000

    threshold on learning rate

--- FlowDesigner/NNet/include/FFLayer.h
+++ FlowDesigner/NNet/include/FFLayer.h
@@ -258,7 +258,9 @@ class FFLayer : public Object {
 	       x=-10;
 	    if (x>10)
 	       x=10;
-	    saved_weights[i] *= .02+1.06/(1+exp((-2*x-2)));
+	    saved_weights[i] *= .02+1.03/(1+exp((-2*x-2)));
+	    if (saved_weights[i] > 10) 
+	       saved_weights[i] = 10;
 	       /*if (momentum[i]*gradient[i] > 0)
 	       saved_weights[i] *= inc;
 	    else
