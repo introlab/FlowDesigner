e86b7d88f1a4baaec410dcb03f469de6196bec50
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2000-06-13 04:23:41 +0000

    ...

--- FlowDesigner/NNet/include/FFLayer.h
+++ FlowDesigner/NNet/include/FFLayer.h
@@ -258,7 +258,7 @@ class FFLayer : public Object {
 	       x=-10;
 	    if (x>10)
 	       x=10;
-	    saved_weights[i] *= .1+.92/(1+exp((-6*x+1)));
+	    saved_weights[i] *= .045+.98/(1+exp((-6*x+2)));
 	    /*if (saved_weights[i] > 10) 
 	       saved_weights[i] = 10;*/
 	       /*if (momentum[i]*gradient[i] > 0)
