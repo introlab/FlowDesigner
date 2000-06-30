08e8d55ac6b79dd638b5ceb7b283e25f85e84ac7
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2000-06-30 04:08:01 +0000

    first type of distortion

---
+++ FlowDesigner/effects/src/Saturate.cc
@@ -0,0 +1,110 @@
+// Copyright (C) 1999 Jean-Marc Valin
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this file.  If not, write to the Free Software Foundation,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#include <stream.h>
+#include "FrameOperation.h"
+#include "Buffer.h"
+#include "Vector.h"
+#include <math.h>
+
+class Saturate;
+
+//DECLARE_NODE(Saturate)
+NODE_INFO(Saturate,"Effects", "INPUT", "OUTPUT", "LENGTH:SATURATION:THRESHOLD")
+
+class Saturate : public FrameOperation {
+   
+   int inputID;
+   int inputLength;
+   enum Type {HARD, TANH, ATAN};
+
+   float threshold;
+   Type saturation;
+
+public:
+   Saturate(string nodeName, ParameterSet params)
+   : FrameOperation(nodeName, params)
+   {
+      inputID = addInput("INPUT");
+      if (parameters.exist("INPUTLENGTH"))
+         inputLength = dereference_cast<int> (parameters.get("INPUTLENGTH"));
+      else inputLength = dereference_cast<int> (parameters.get("LENGTH"));
+
+      threshold = dereference_cast<float> (parameters.get("THRESHOLD"));
+      
+      if (parameters.exist("INPUTLENGTH"))
+      {
+	 String satur = object_cast<String> (parameters.get("SATURATION"));
+	 if (satur == "hard")
+	    saturation = HARD;
+	 else if (satur == "tanh")
+	    saturation = TANH;
+	 else if (satur == "atan")
+	    saturation = ATAN;
+	 else saturation = HARD;
+      } else saturation = HARD;
+   }
+
+   ~Saturate() {}
+
+   virtual void specificInitialize()
+   {
+      this->FrameOperation::specificInitialize();
+   }
+
+   void calculate(int output_id, int count, Buffer &out)
+   {
+      NodeInput input = inputs[inputID];
+      ObjectRef inputValue = input.node->getOutput(input.outputID, count);
+
+      Vector<float> &output = object_cast<Vector<float> > (out[count]);
+      if (inputValue->status != Object::valid)
+      {
+         output.status = inputValue->status;
+         return;
+      }
+      const Vector<float> &in = object_cast<Vector<float> > (inputValue);
+      
+      switch (saturation)
+      {
+	 case HARD:
+	    for (int i=0;i<outputLength;i++)
+	    {
+	       output[i] = in[i];
+	       if (output[i] < -threshold)
+		  output[i] = -threshold;
+	       else if (output[i] > threshold)
+		  output[i] = threshold;
+	    }
+	    break;
+	 case ATAN:
+	    for (int i=0;i<outputLength;i++)
+	    {
+	       output[i] = ((2/M_PI)*threshold) * atan(in[i]/threshold);
+	    }
+	    break;
+	 case TANH:
+	    for (int i=0;i<outputLength;i++)
+	    {
+	       output[i] = threshold * tanh(in[i]/threshold);
+	    }
+	    break;
+      }
+      
+      output.status = Object::valid;
+   }
+
+};
