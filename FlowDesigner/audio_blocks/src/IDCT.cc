ca3db77cdce11fbdee77cbc21e9b061247fd7131
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2000-08-29 04:35:40 +0000

    converted nodes to new vector allocation scheme and made an abstract FFT layer

--- FlowDesigner/audio_blocks/src/IDCT.cc
+++ FlowDesigner/audio_blocks/src/IDCT.cc
@@ -15,109 +15,90 @@
 // 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 #include <stream.h>
-#include "FrameOperation.h"
+#include "BufferedNode.h"
 #include "Buffer.h"
 #include "Vector.h"
-#include <fftw.h>
-#include <fftw.h>
 #include <math.h>
+#include "FFTWrap.h"
 
 class IDCT;
 
-//DECLARE_NODE(IDCT)
 NODE_INFO(IDCT, "Signal:DSP", "INPUT", "OUTPUT", "LENGTH")
 
-class IDCT : public FrameOperation {
+class IDCT : public BufferedNode {
    
    int inputID;
-   int inputLength;
+   int outputID;
+   int length;
 
-   fftw_plan plan;
-   fftw_complex *inputCopy;
-   fftw_complex *outputCopy;
    float *rNormalize;
    float *iNormalize;
 
 public:
    IDCT(string nodeName, ParameterSet params)
-      : FrameOperation(nodeName, params)
+      : BufferedNode(nodeName, params)
 
    {
       inputID = addInput("INPUT");
-      if (parameters.exist("INPUTLENGTH"))
-         inputLength = dereference_cast<int> (parameters.get("INPUTLENGTH"));
-      else inputLength = dereference_cast<int> (parameters.get("LENGTH"));
+      outputID = addOutput("OUTPUT");
+      length = dereference_cast<int> (parameters.get("LENGTH"));
 
-      if (inputLength & 1) 
+      if (length & 1) 
       {
 	 throw NodeException(NULL, "IDCT only implemented for even sizes", __FILE__, __LINE__);
       }
 
-      inputCopy = new fftw_complex [inputLength];
-      outputCopy =new fftw_complex [inputLength];
-      rNormalize =new float [inputLength];
-      iNormalize =new float [inputLength];
-      float sqrt2n=sqrt(2.0/inputLength);
-      for (int i=0;i<inputLength;i++)
+      inputCopy = new fftw_complex [length];
+      outputCopy =new fftw_complex [length];
+      rNormalize =new float [length];
+      iNormalize =new float [length];
+      float sqrt2n=sqrt(2.0/length);
+      for (int i=0;i<length;i++)
       {
-	 rNormalize[i]=cos(M_PI*i/(2*inputLength))*sqrt2n;
-	 iNormalize[i]=sin(M_PI*i/(2*inputLength))*sqrt2n;
+	 rNormalize[i]=cos(M_PI*i/(2*length))*sqrt2n;
+	 iNormalize[i]=sin(M_PI*i/(2*length))*sqrt2n;
       }
       rNormalize[0] /= sqrt(2);
-
-      //normalize = .5/inputLength;
-      plan = fftw_create_plan (inputLength, FFTW_BACKWARD, FFTW_ESTIMATE);
 }
 
    ~IDCT() 
    {
-      fftw_destroy_plan(plan);
       delete [] inputCopy;
       delete [] outputCopy;
       delete [] rNormalize;
       delete [] iNormalize;
    }
 
-   virtual void specificInitialize()
-   {
-      this->FrameOperation::specificInitialize();
-      
-   }
-
    void calculate(int output_id, int count, Buffer &out)
    {
-      NodeInput input = inputs[inputID];
-      ObjectRef inputValue = input.node->getOutput(input.outputID, count);
+      ObjectRef inputValue = getInput(inputID, count);
 
-      Vector<float> &output = object_cast<Vector<float> > (out[count]);
       if (inputValue->status != Object::valid)
       {
-         output.status = inputValue->status;
+	 out[count] = inputValue;
          return;
       }
       const Vector<float> &in = object_cast<Vector<float> > (inputValue);
-      
+
+      Vector<float> &output = *Vector<float>::alloc(length);
+      out[count] = &output;
+            
       int i,j;
 
 
-      for (i=0;i<inputLength;i++)
+      for (i=0;i<length;i++)
       {
 	 inputCopy[i].re = rNormalize[i]*in[i];
 	 inputCopy[i].im = iNormalize[i]*in[i];
-	 //inputCopy[inputLength-i] = iNormalize[i]*in[i];
       }
 
-      fftw_one(plan, inputCopy, outputCopy);
+      FFTWrap.ifft(inputCopy, outputCopy, length);
 
-
-      for (i=0, j=0 ;i<inputLength ; i+=2, j++)
+      for (i=0, j=0 ;i<length ; i+=2, j++)
          output[i]=outputCopy[j].re;
 
-      for (i = inputLength-1; i>=0 ; i-=2, j++)
+      for (i = length-1; i>=0 ; i-=2, j++)
          output[i]=outputCopy[j].re;
-
-
-      output.status = Object::valid;
    }
 
 };
