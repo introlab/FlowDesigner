d36251ea387addad6c8239c29caa5ad89cd60b74
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2000-11-17 01:27:12 +0000

    cleaning up again...

--- FlowDesigner/audio_blocks/src/LPC.cc
+++ FlowDesigner/audio_blocks/src/LPC.cc
@@ -15,7 +15,7 @@
 // 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 #include <stream.h>
-#include "FrameOperation.h"
+#include "BufferedNode.h"
 #include "Buffer.h"
 #include "Vector.h"
 #include "lpc.h"
