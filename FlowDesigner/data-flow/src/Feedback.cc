6a1941975d16f5f5352302e815ca233a95ecc3d7
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-02-23 04:44:29 +0000

    Feedback is kludged, but it works in some cases!!!

--- FlowDesigner/data-flow/src/Feedback.cc
+++ FlowDesigner/data-flow/src/Feedback.cc
@@ -14,7 +14,7 @@
 // along with this file.  If not, write to the Free Software Foundation,
 // 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-#include "Node.h"
+#include "Node2.h"
 
 class Feedback;
 
@@ -31,14 +31,22 @@ DECLARE_NODE(Feedback)
  * @output_name OUTPUT
  * @output_description No description available
  *
+ * @output_name DELAY
+ * @output_description No description available
+ *
+ * @parameter_name DELAY
+ * @parameter_description No description available
+ *
 END*/
 
 
 class Feedback : public Node {
 protected:
    int inputID;
+   int delayID;
    int outputID;
    bool insideRequest;
+   int delay;
 
 public:
    Feedback(string nodeName, ParameterSet params)
@@ -48,12 +56,23 @@ public:
       try {
          inputID = addInput("INPUT");
 	 outputID=addOutput("OUTPUT");
+	 delayID = addOutput("DELAY");
+	 delay = dereference_cast<int> (parameters.get ("DELAY"));
       } catch (BaseException *e)
       {
          //e->print();
          throw e->add(new NodeException (NULL, "Exception caught in Feedback constructor", __FILE__, __LINE__));
       }
       
+   }
+
+   void specificInitialize()
+   {
+      Node::specificInitialize();
+      ParameterSet req;
+      req.add("LOOKBACK", ObjectRef(new Int(delay)));
+      req.add("INORDER", Object::nilObject);
+      inputs[inputID].node->request(inputs[inputID].outputID,req);
    }
 
    ~Feedback() 
@@ -66,7 +85,8 @@ public:
    void Feedback::initialize ()
    {
       if (initialized) return;
-      if (--outputInitializeCount <=0)
+      //FIXME: This is a big kludge, it won't work if more than 1 link is connected to "OUTPUT"
+      if (1 || --outputInitializeCount <=0)
       {
 	 
 	 specificInitialize();
@@ -83,8 +103,7 @@ public:
 	       if (!in->node->hasOutput(in->outputID)) 
 		  throw new NodeException(this, "Input node doesn't implement output", __FILE__, __LINE__);
 	       try {
-		  //Deliberatly not propagating the initialization, to avoid infinite recursion
-		  //in->node->initialize();
+		  in->node->initialize();
 	       } catch (BaseException *e)
 	       {
 		  throw e->add(new NodeException(this, "Exception caught in node initialization", __FILE__, __LINE__));
@@ -93,55 +112,27 @@ public:
 	 }
 	 
       }
-   }
-      
-      
-   /*Warning: overriding the initialize function should not be done unless you REALLY 
-              know what you're doing... and I'm not even sure I know what 
-              I'm doing here*/
-   void Feedback::connectToNode(unsigned int in, Node *inputNode, unsigned int out)
+   }      
+
+   /**Modified the request passing method in order to avoid strange behaviours*/
+      virtual void request(int output_id, const ParameterSet &req) 
    {
-
-      
-      if (inputs.size() <= in) {
-	 //the old method was used for adding inputs
-	 throw NodeException(this, "We shouldn't be initializing this way anymore", __FILE__, __LINE__);
-	 inputs.resize(in + 1);
-	 inputs[in] = NodeInput(inputNode,out,string());
-      }
-      else {
-	 //the new method was used for adding inputs
-	 inputs[in].outputID = out;
-	 inputs[in].node = inputNode;
-	 /*Here, I'm deliberately not registering to the input node in order to avoir an 
-	   initialization problem*/
-	 //inputNode->registerOutput(out);
-      }
+      if (output_id == outputID)
+	 inputs[inputID].node->request(inputs[inputID].outputID,req);
    }
-      
-
-   /**Modified the request passing method in order to avoir an infinite recursion*/
-   virtual void request(int outputID, const ParameterSet &req) 
-   {
-      //if insideRequest is true, it means we're being called recursively... the recursion must stop
-      if (!insideRequest)
-      {
-	 insideRequest=true;
-	 ParameterSet req2(req);
-	 cerr << "Got request\n";
-	 req2.add("INORDER", Object::nilObject);
-	 inputs[inputID].node->request(inputs[inputID].outputID,req2);
-	 cerr << "Sent request\n";
-	 insideRequest=false;
-      }
-   }
-
       
    ObjectRef getOutput(int output_id, int count)
    {
-      if (count < 0)
-	 return Object::before_beginningObject;
-      return getInput(inputID, count);
+      if (output_id == outputID)
+	 return getInput(inputID, count);
+      else if (output_id == delayID)
+      {
+	 if (count-delay < 0)
+	    return Object::before_beginningObject;
+	 return getInput(inputID, count-delay);
+      } else {
+	 //error
+      }
    }
       
 };
