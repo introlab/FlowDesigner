e9ff1d2cc32ac4ba802f4d8f793a883497ac3809
maestro <maestro@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-02-23 13:55:32 +0000

    FeedBack Node.h

--- FlowDesigner/data-flow/src/Feedback.cc
+++ FlowDesigner/data-flow/src/Feedback.cc
@@ -14,7 +14,7 @@
 // along with this file.  If not, write to the Free Software Foundation,
 // 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-#include "Node2.h"
+#include "Node.h"
 
 class Feedback;
 
