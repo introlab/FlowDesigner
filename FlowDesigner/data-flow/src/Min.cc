5e9d81654f81921e41d1ede295b8b16969a74803
maestro <maestro@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-03-06 19:57:28 +0000

    added Math::MaxValue and Math::MinValue

---
+++ FlowDesigner/data-flow/src/Min.cc
@@ -0,0 +1,89 @@
+// Copyright (C) 2001 Jean-Marc Valin & Dominic Letourneau
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this file.  If not, write to the Free Software Foundation,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#include "BufferedNode.h"
+#include "operators.h"
+
+class MinValue;
+DECLARE_NODE(MinValue)
+/*Node
+ *
+ * @name MinValue
+ * @category Math
+ * @description No description available
+ *
+ * @input_name INPUT1
+ * @input_description No description available
+ *
+ * @input_name INPUT2
+ * @input_description No description available
+ *
+ * @output_name OUTPUT
+ * @output_description No description available
+ *
+END*/
+
+
+class MinValue : public BufferedNode {
+protected:
+   ///The ID of the 'output' output
+   int outputID;
+
+   ///The ID of the 'input1' input
+   int input1ID;
+
+   ///The ID of the 'input2' input
+   int input2ID;
+
+public:
+   ///Constructor, takes the name of the node and a set of parameters
+   MinValue(string nodeName, ParameterSet params) 
+     : BufferedNode(nodeName, params)
+   {
+      input1ID = addInput ("INPUT1");
+      input2ID = addInput ("INPUT2");
+      outputID = addOutput ("OUTPUT"); 
+   }
+   
+
+
+  void calculate(int output_id, int count, Buffer &out) {
+
+     ObjectRef InputValue1 = getInput(input1ID,count);
+     
+     if (InputValue1->status != Object::valid) {
+       out[count] = InputValue1;
+       return;
+     }
+     
+     ObjectRef InputValue2 = getInput(input2ID,count);
+
+     if (InputValue2->status != Object::valid) {
+       out[count] = InputValue2;
+       return;
+     }
+
+     
+     if ( dereference_cast<bool>(InputValue1 > InputValue2)) {
+       out[count] = InputValue2;
+     }
+     else {
+       out[count] = InputValue1;
+     }
+
+   }
+   
+};
