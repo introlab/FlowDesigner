24cc8f74b2c47ae1a362cfa0e5cebea8efd86bb3
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-04-06 02:22:17 +0000

    ...

--- FlowDesigner/data-flow/include/vmethod.h
+++ FlowDesigner/data-flow/include/vmethod.h
@@ -2,10 +2,25 @@
 #include <map>
 #include "Object.h"
 
-class VirtualMethod {
+class SymbolSet {
   protected:
-   static map<string,int> &translationMap();
+   int currentID;
+   map<string,int> translationMap;
   public:
-  
+   SymbolSet()
+      : currentID(0)
+   {}
+   int get(string str)
+   {
+      map<string,int>::iterator sym = translationMap.find(str);
+      if (sym == translationMap.end())
+      {
+	 translationMap.insert(make_pair(str, currentID++));
+      } else {
+	 return sym->second;
+      }
+   }
    
 };
+
+extern SymbolSet* symbols;
