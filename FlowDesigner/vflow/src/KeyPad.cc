2e807824ef1860b300bdaca545d604ef3e4e6a7f
maestro <maestro@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-04-22 02:48:15 +0000

    Copyright year = 2001

--- FlowDesigner/vflow/src/KeyPad.cc
+++ FlowDesigner/vflow/src/KeyPad.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2000 Dominic Letourneau
+// Copyright (C) 2001 Dominic Letourneau
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
