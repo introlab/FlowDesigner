62d86fc1d9c006ebaaebb6ea156d8cc135a735bb
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-04-24 22:48:13 +0000

    extended functionality...

--- FlowDesigner/data-flow/src/Float2Vect.cc
+++ FlowDesigner/data-flow/src/Float2Vect.cc
@@ -25,15 +25,23 @@ DECLARE_NODE(Float2Vect)
  *
  * @name Float2Vect
  * @category Vector
- * @description Converts a float value to a vector of 1 element
+ * @description Converts float values to a vector of elements in past and future
  *
  * @input_name INPUT
- * @input_description The input float 
  * @input_type float
+ * @input_description The input float 
  *
  * @output_name OUTPUT
- * @output_description The vector
  * @output_type Vector
+ * @output_description The vector
+ *
+ * @parameter_name LOOKAHEAD
+ * @parameter_type int
+ * @parameter_description Number of elements in the future
+ *
+ * @parameter_name LOOKBACK
+ * @parameter_type int
+ * @parameter_description Number of elements in the past
  *
 END*/
 
@@ -42,30 +50,51 @@ class Float2Vect : public BufferedNode {
    
    int inputID;
    int outputID;
+   int lookAhead;
+   int lookBack;
 
 public:
    Float2Vect(string nodeName, ParameterSet params)
-   : BufferedNode(nodeName, params)
+      : BufferedNode(nodeName, params)
+      , lookAhead(0)
+      , lookBack(0)
    {
       inputID = addInput("INPUT");
       outputID = addOutput("OUTPUT");
+
+      if (parameters.exist("LOOKAHEAD"))
+	 lookAhead = dereference_cast<int> (parameters.get("LOOKAHEAD"));
+      if (parameters.exist("LOOKBACK"))
+	 lookBack = dereference_cast<int> (parameters.get("LOOKBACK"));
+      inputsCache[inputID].lookAhead = lookAhead;
+      inputsCache[inputID].lookBack = lookBack;
    }
 
    void calculate(int output_id, int count, Buffer &out)
    {
-      ObjectRef inputValue = getInput(inputID, count);
-
-      if (inputValue->status != Object::valid)
-      {
-	 out[count] = inputValue;
-         return;
-      }
-      const float &in = dereference_cast<float> (inputValue);
-
-      Vector<float> &output = *Vector<float>::alloc(1);
+      Vector<float> &output = *Vector<float>::alloc(1 + lookAhead + lookBack);
       out[count] = &output;
-      output[0] = in;
 
+      for (int i=-lookBack;i<=lookAhead;i++)
+      {
+	 if (count+i>=0)
+	 {
+	    ObjectRef inputValue = getInput(inputID, count+i);
+	    if (inputValue->status != Object::valid)
+	    {
+	       //out[count] = inputValue;
+	       //return;
+	       output[lookBack+i] = 0;
+	    } else {
+	       output[lookBack+i] = dereference_cast<float> (inputValue);
+	    }
+	    
+	 } else {
+	    //out[count] = Object::before_beginningObject;
+	    //return;
+	    output[lookBack+i] = 0;
+	 }
+      }
       
    }
 
