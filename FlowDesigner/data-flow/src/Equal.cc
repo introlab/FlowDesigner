bd3bacfcaee7de6ad2df9586a739862393d003bc
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-04-27 14:00:28 +0000

    Equal operator

---
+++ FlowDesigner/data-flow/src/Equal.cc
@@ -0,0 +1,75 @@
+// Copyright (C) 1999 Jean-Marc Valin
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this file.  If not, write to the Free Software Foundation,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#include "BufferedNode.h"
+#include "Buffer.h"
+#include "operators.h"
+
+class Equal;
+
+DECLARE_NODE(Equal)
+/*Node
+ *
+ * @name Equal
+ * @category Math
+ * @description Equals two input values and returns the result
+ *
+ * @input_name INPUT1
+ * @input_description First value
+ *
+ * @input_name INPUT2
+ * @input_description Second value
+ *
+ * @output_name OUTPUT
+ * @output_description Result of the addition
+ *
+END*/
+
+
+class Equal : public BufferedNode {
+   
+   int input1ID;
+   int input2ID;
+   int outputID;
+
+public:
+   Equal(string nodeName, ParameterSet params)
+   : BufferedNode(nodeName, params)
+   {
+      input1ID = addInput("INPUT1");
+      input2ID = addInput("INPUT2");
+      outputID = addOutput("OUTPUT");
+   }
+
+   void calculate(int output_id, int count, Buffer &out)
+   {
+      ObjectRef inputValue = getInput(input1ID, count);
+      if (inputValue->status != Object::valid)
+      {
+	 out[count] = inputValue;
+         return;
+      }
+
+      ObjectRef input2Value = getInput(input2ID, count);
+      if (input2Value->status != Object::valid)
+      {
+	 out[count] = input2Value;
+	 return;
+      }
+      out[count] = inputValue == input2Value;
+   }
+
+};
