8793cbfe8624b292a66a3cac6e15dcba833b11fb
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-05-24 21:10:35 +0000

    ...

--- FlowDesigner/audio_blocks/include/FFTWrap.h
+++ FlowDesigner/audio_blocks/include/FFTWrap.h
@@ -23,6 +23,9 @@ using namespace std;
 #ifdef HAVE_FFTW
 #include <fftw.h>
 #include <rfftw.h>
+
+#if (FFTW_ENABLE_FLOAT == 1)
+
 typedef fftw_complex fft_complex;
 
 
@@ -122,6 +125,114 @@ class _FFTWrap {
 
    
 };
+#else /* (FFTW_ENABLE_FLOAT == 1) */
+
+#error You do not have FFTW compiled with the --enable-float option. Either compile it with --enable-float or do not use it (you won't be able to compute FFT's)
+/*
+typedef fftw_complex fft_complex;
+
+
+#ifdef NO_HASH_MAP
+#include <map>
+#else
+#ifdef HAVE_HASH_MAP
+#include <hash_map>
+#elif defined (HAVE_EXT_HASH_MAP)
+#include <ext/hash_map>
+#endif
+#endif
+
+class _FFTWrap {
+#ifdef NO_HASH_MAP
+   typedef map<int, rfftw_plan> FFTPlanMap;
+   typedef map<int, rfftw_plan> RFFTPlanMap;
+#else
+   typedef hash_map<int, rfftw_plan, hash<int> > FFTPlanMap;
+   typedef hash_map<int, rfftw_plan, hash<int> > RFFTPlanMap;
+#endif
+   
+   FFTPlanMap FFTPlans[2];
+   RFFTPlanMap RFFTPlans[2];
+  public:
+
+   ~_FFTWrap() 
+   {
+      for (int i=0;i<2;i++)
+	 for (RFFTPlanMap::iterator plan_pair = RFFTPlans[i].begin(); plan_pair != RFFTPlans[i].end(); plan_pair++)
+	    rfftw_destroy_plan(plan_pair->second);
+      for (int i=0;i<2;i++)
+	 for (FFTPlanMap::iterator plan_pair = FFTPlans[i].begin(); plan_pair != FFTPlans[i].end(); plan_pair++)
+	    fftw_destroy_plan(plan_pair->second);
+   }
+
+   void fft (const complex<float> *fin, complex<float> *fout, int size)
+   {
+      FFTPlanMap::iterator plan_pair = FFTPlans[0].find(size);
+      fftw_plan *plan;
+      if (plan_pair == FFTPlans[0].end())
+      {
+	 FFTPlans[0][size] = fftw_create_plan (size, FFTW_FORWARD, FFTW_ESTIMATE);
+	 plan = &FFTPlans[0][size];
+      } else {
+	 plan = &plan_pair->second;
+      }
+      
+      fftw_one (*plan, const_cast <FFTW_COMPLEX *> (in), out);
+   }
+
+   void ifft (const float *fin, float *fout, int size)
+   {
+      FFTPlanMap::iterator plan_pair = FFTPlans[1].find(size);
+      fftw_plan *plan;
+      if (plan_pair == FFTPlans[1].end())
+      {
+	 FFTPlans[1][size] = fftw_create_plan (size, FFTW_BACKWARD, FFTW_ESTIMATE);
+	 plan = &FFTPlans[1][size];
+      } else {
+	 plan = &plan_pair->second;
+      }
+      
+      fftw_one (*plan, const_cast <FFTW_COMPLEX *> (in), out);
+   }
+
+   void rfft (const float *fin, float *fout, int size)
+   {
+      RFFTPlanMap::iterator plan_pair = RFFTPlans[0].find(size);
+      rfftw_plan *plan;
+      if (plan_pair == RFFTPlans[0].end())
+      {
+	 RFFTPlans[0][size] = rfftw_create_plan (size, FFTW_FORWARD, FFTW_ESTIMATE);
+	 plan = &RFFTPlans[0][size];
+      } else {
+	 plan = &plan_pair->second;
+      }
+      
+      rfftw_one (*plan, const_cast <float *> (in), out);
+   }
+
+   void irfft (const float *fin, float *fout, int size)
+   {
+      RFFTPlanMap::iterator plan_pair = RFFTPlans[1].find(size);
+      rfftw_plan *plan;
+      if (plan_pair == RFFTPlans[1].end())
+      {
+	 RFFTPlans[1][size] = rfftw_create_plan (size, FFTW_BACKWARD, FFTW_ESTIMATE);
+	 plan = &RFFTPlans[1][size];
+      } else {
+	 plan = &plan_pair->second;
+      }
+      
+      rfftw_one (*plan, const_cast <float *> (in), out);
+
+   }
+
+   
+};
+*/
+
+#endif /* (FFTW_ENABLE_FLOAT == 1) */
+
+
 #else /* ifdef HAVE_FFTW */
 
 #include <iostream>
