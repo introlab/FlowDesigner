be91bea0124203d8759d0ced678c65d5f00f44ce
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-05-31 01:40:19 +0000

    A bit more comments!

--- FlowDesigner/NNet/include/FFLayer.h
+++ FlowDesigner/NNet/include/FFLayer.h
@@ -10,36 +10,62 @@
 #include "functions.h"
 
 
-/**Represents one layer from a multi-layer percetron (FFNet)*/
+/**Represents one (fully-connected) layer from a multi-layer percetron (FFNet)
+   @author: Jean-Marc Valin
+*/
 class FFLayer : public Object {
   public:
+   /**Activation function pointer*/
    void (*func) (float *, float *, int);
+
+   /**Activation function derivative pointer*/
    void (*deriv_func) (float *, float *, int);
+
   protected:
+   /**Number of neurons in the layer*/
    int nbNeurons;
+
+   /**Number of input neurons*/
    int nbInputs;
+
+   /**Pointer to the weight vector*/
    float *weights;
+
+   /*type of activation function (tansig, sigmoid, lin)*/
    string funcType;
-   int weightOffset, neuronOffset;
+
+   /**Offset of the layer weight vector in the network weight vector*/
+   int weightOffset;
+
+   /**Offset of the layer's first neuron in the list of all network neurons 
+      (not too sure about that one, though)*/
+   int neuronOffset;
+
+   /**Offset of the layer's first neuron derivative in the list of all network neurons*/
    float derivOffset;
 
   public:
+   /**This (empty) constructor is used for parsing a layer from a file*/
    FFLayer() 
       : derivOffset(0) 
    {}
 
    //FFLayer(float *_weights) : weights(_weights) {};
 
+   /**Standard constructor*/
    FFLayer(int _nbNeurons, int _nbInputs, float *_weights, int _weightOffset, int _neuronOffset, string type = "tansig");
 
+   /**Unimplemented yet (not sure if we *should* implement it)*/
    FFLayer(const FFLayer &layer) {cerr << "I wouldn't do that if I were you\n";}
    
+   /**Called after reading a layer to setup the weight vector correctly*/
    void setupAfterRead(float *_weights, int _weightOffset, int _neuronOffset);
 
    ~FFLayer() 
    {
    }
 
+   /**Calculates all the activation functions (and derivatives) for a given input*/
    void update(const float *previous, float *value, float *deriv=NULL)
    {
       for (int i=0;i<nbNeurons;i++)
@@ -74,10 +100,14 @@ class FFLayer : public Object {
 	 vec_add_scal(derivOffset, deriv, deriv, nbNeurons);
    }
 
+   /**Layer size*/
    int size() {return nbNeurons;}
 
+   /**Number of weights*/
    int getNbWeights() {return nbNeurons*(nbInputs+1);}
 
+   /**Offset of a certain neuron's weight vector relative to the whole network's
+      weight vector*/
    int getNeuronWeightOffset(int i) {return weightOffset+i*(nbInputs+1);}
 
    int getWeightOffset() {return weightOffset;}
@@ -86,19 +116,25 @@ class FFLayer : public Object {
 
    void init(float minmax);
 
+   /**Initializes the layer values given the means and standard deviations of each 
+      of the inputs*/
    void init(float *mean, float *std);
 
+   /**Sets the bias vector*/
    void setBias(float *minmax);
 
    float *getWeights(int i) {return weights + i*(nbInputs+1);}
 
+   /**Writes the layer to a stream*/
    void printOn(ostream &out) const;
 
+   /**Reads the layer from a stream (remember to call setupAfterRead after that*/
    void readFrom (istream &in);
 
    void setDerivOffset(float d) {derivOffset=d;}
 };
 
+/**Extraction operator for FFLayer*/
 istream &operator >> (istream &in, FFLayer &layer);
 
 #endif
