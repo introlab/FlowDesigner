d900369c35355e5318d8be6be25ee5636d445499
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-06-07 03:49:38 +0000

    For Piper source compatibility

--- FlowDesigner/data-flow/include/UINetwork.h
+++ FlowDesigner/data-flow/include/UINetwork.h
@@ -136,7 +136,7 @@ public:
 
 //   virtual UITerminal *newTerminal (string _name, UINode *_node, bool _isInput, double _x, double _y);
 
-   virtual UILink *newLink (UITerminal *_from, UITerminal *_to, char *str);
+   virtual UILink *newLink (UITerminal *_from, UITerminal *_to, char *str=NULL);
 
    virtual UINetTerminal *newNetTerminal (UITerminal *_terminal, UINetTerminal::NetTermType _type, string _name);
 
