dc07e1aba8f37e924a3d3a2e379a5095463f6337
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-07-11 18:50:10 +0000

    ...

--- FlowDesigner/data-flow/src/codeflow.cc
+++ FlowDesigner/data-flow/src/codeflow.cc
@@ -8,25 +8,31 @@
 
 int main(int argc, char **argv)
 {
-
-  try {
-    scanDL();
-    UIDocument::loadAllInfo();
-    UIDocument *doc = new UIDocument(argv[1]);
-    doc->load();
-    ofstream out("code.cc");
-    doc->genCode(out, "buildDoc");
-  }
-  catch (BaseException *e) 
-  {
-     e->print();
-  }  
-  catch (...) {
-    cerr<<"Unhandled exception in "<<argv[0]<<endl;
-    cerr<<"Exiting"<<endl;
-    exit(-1);
-  }
-
-  return 0;
+   if (argc!=4)
+   {
+      cerr << "usage: codeflow  <input XML file>  <output C++ source file>  <build function name>" << endl;
+      exit(1);
+   }
+   
+   try 
+   {
+      scanDL();
+      UIDocument::loadAllInfo();
+      UIDocument *doc = new UIDocument(argv[1]);
+      doc->load();
+      ofstream out(argv[2]);
+      doc->genCode(out, argv[3]);
+   }
+   catch (BaseException *e) 
+   {
+      e->print();
+   }  
+   catch (...) {
+      cerr<<"Unhandled exception in "<<argv[0]<<endl;
+      cerr<<"Exiting"<<endl;
+      exit(-1);
+   }
+   
+   return 0;
 }
 
