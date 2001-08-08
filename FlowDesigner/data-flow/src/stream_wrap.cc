5dacc061acaaa1ad2c7fdf7f950795f10129385d
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-08-08 16:57:44 +0000

    ...

--- FlowDesigner/data-flow/src/stream_wrap.cc
+++ FlowDesigner/data-flow/src/stream_wrap.cc
@@ -147,12 +147,12 @@ streamsize fd_streambuf::xsgetn(char *s, streamsize n)
 
 #endif
 
-
+/*
 int main() 
 {
    fd_ostream out(1);
    //fileptr_ostream out(stdout);
    out.write("salut\n", 6);
 }
-
+*/
 
