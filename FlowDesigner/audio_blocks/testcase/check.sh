253be8446e45f6fe2d1a02b7359618c0d632570d
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-08-14 03:12:10 +0000

    DCT testcase

---
+++ FlowDesigner/audio_blocks/testcase/check.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+for file in *.n non-existant.n
+	do
+	if test -f $file.out
+		then
+		if batchflow $file | grep -qf $file.out
+			then
+			echo $file passed
+		else
+			echo $file failed
+		fi
+	else
+		batchflow $file > /dev/null 2>&1
+		if test $? -lt 2
+			then
+			echo $file passed
+		else
+			echo $file failed
+		fi
+	fi
+done
