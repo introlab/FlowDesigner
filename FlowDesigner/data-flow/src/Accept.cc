5574efb3ccb8a3c8d1357b21a5da8f863840f5e7
maestro <maestro@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-09-19 17:00:35 +0000

    added Network Accept Node

---
+++ FlowDesigner/data-flow/src/Accept.cc
@@ -0,0 +1,62 @@
+// Copyright (C) 2001 Dominic Letourneau
+
+#include "BufferedNode.h"
+#include "operators.h"
+#include "NetworkSocket.h"
+
+class Accept;
+
+DECLARE_NODE(Accept)
+/*Node
+ *
+ * @name Accept
+ * @category Network
+ * @description Create a network socket of any type
+ *
+ * @input_name SOCKET
+ * @input_description The socket to listen to
+ *
+ * @output_name SOCKET
+ * @output_description The socket to be used for input/output operations
+ *
+END*/
+
+
+class Accept : public BufferedNode {
+   
+  int inputID;
+  int outputID;
+
+public:
+
+   Accept(string nodeName, ParameterSet params)
+   : BufferedNode(nodeName, params) {
+
+     inputID = addInput("SOCKET");
+     outputID = addOutput("SOCKET");
+   }
+
+   void calculate(int output_id, int count, Buffer &out) {
+
+
+     ObjectRef socketValue = getInput(inputID,count);
+
+     NetworkSocket &my_socket = object_cast<NetworkSocket>(socketValue);
+
+     if (my_socket.get_type() == NetworkSocket::TCP_SERVER_STREAM_TYPE) {
+
+       //backlog = 1
+       my_socket.server_listen(1);
+
+       my_socket.server_accept();
+
+     }
+     else {
+       throw new GeneralException("NetworkSocket is not of type TCP_SERVER_STREAM_TYPE",__FILE__,__LINE__);
+     }
+
+     out[count] = socketValue;
+
+   }
+
+};
