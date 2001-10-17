0946549f26c9186a66afc8f3489ee0313a651cb6
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-10-17 16:42:16 +0000

    Covariance computation

---
+++ FlowDesigner/audio_blocks/src/CovarianceAccum.cc
@@ -0,0 +1,72 @@
+// Copyright (C) 1999 Jean-Marc Valin
+
+#include "BufferedNode.h"
+#include "Buffer.h"
+#include "Vector.h"
+#include "vec.h"
+#include "Matrix.h"
+
+class CovarianceAccum;
+
+DECLARE_NODE(CovarianceAccum)
+/*Node
+ *
+ * @name CovarianceAccum
+ * @category Matrix
+ * @description Updates (accumulate) a covariance matrix with an observation vector
+ *
+ * @input_name INPUT
+ * @input_type Vector<float>
+ * @input_description Input (observation) vector
+ *
+ * @input_name MATRIX
+ * @input_type Matrix<float>
+ * @input_description Input (covariance) matrix
+ *
+ * @output_name OUTPUT
+ * @output_type Matrix<float>
+ * @output_description Updated matrix (same object as input)
+ *
+END*/
+
+class CovarianceAccum : public BufferedNode {
+   
+   int inputID;
+   int matrixID;
+   int outputID;
+
+public:
+   CovarianceAccum(string nodeName, ParameterSet params)
+   : BufferedNode(nodeName, params)
+   {
+      inputID = addInput("INPUT");
+      matrixID = addInput("MATRIX");
+      outputID = addOutput("OUTPUT");
+      inOrder = true;
+   }
+
+   void calculate(int output_id, int count, Buffer &out)
+   {
+      ObjectRef inputValue = getInput(inputID, count);
+      ObjectRef matrixValue = getInput(matrixID, count);
+
+      const Vector<float> &in = object_cast<Vector<float> > (inputValue);
+      Matrix<float> &mat = object_cast<Matrix<float> > (matrixValue);
+      int length = in.size();
+      if (length != mat.ncols() || length != mat.nrows())
+	 throw new NodeException(this, "Covariance matrix must be square and have same size as input vector", 
+				 __FILE__, __LINE__);
+
+      out[count] = matrixValue;
+      for (int i=0;i<length;i++)
+      {
+	 for (int j=i+1;j<length;j++)
+	 {
+	    mat[i][j] += in[i]*in[j];
+	    mat[j][i] += in[i]*in[j];
+	 }
+	 mat[i][i] += in[i]*in[i];
+      }
+   }
+      
+};
---
+++ FlowDesigner/data-flow/src/DCMatrix.cc
@@ -0,0 +1,81 @@
+//Copyright (C) 2001 Locus Dialog 
+//Author: Jean-Marc Valin
+
+
+#include "Node.h"
+#include "Matrix.h"
+#include "ObjectParser.h"
+#include <iostream>
+#include <sstream>
+
+class DCMatrix;
+
+DECLARE_NODE(DCMatrix)
+/*Node
+ *
+ * @name DCMatrix
+ * @category Matrix
+ * @description Creates a matrix of identical values
+ *
+ * @output_name OUTPUT
+ * @output_type Matrix<float>
+ * @output_description The matrix
+ *
+ * @parameter_name ROWS
+ * @parameter_type int
+ * @parameter_description Number of rows
+ *
+ * @parameter_name COLUMNS
+ * @parameter_type int
+ * @parameter_description Number of columns
+ *
+ * @parameter_name VALUE
+ * @parameter_type float
+ * @parameter_description Value of each element
+ *
+END*/
+
+
+/** A constant node contains a value that will never changes. */
+class DCMatrix : public Node
+{
+
+protected:
+
+   /**The value of the constant*/
+   ObjectRef value;
+
+   /**The ID of the 'value' output*/
+   int outputID;
+public:
+
+   /**Constructor, takes the name of the node and a set of parameters*/
+   DCMatrix(string nodeName, ParameterSet params)
+      : Node(nodeName, params) 
+      //, value (parameters.get("VALUE"))
+   {
+      outputID = addOutput("OUTPUT");
+      int rows = dereference_cast<int> (parameters.get("ROWS"));
+      int cols = dereference_cast<int> (parameters.get("COLUMNS"));
+      
+      value = ObjectRef(new Matrix<float>(rows, cols));
+      Matrix<float> &val = object_cast<Matrix<float> > (value);
+      float value = dereference_cast<float> (parameters.get("VALUE"));;
+      for (int i=0;i<rows;i++)
+	 for (int j=0;j<cols;j++)
+	    val[i][j]=value;
+   }
+
+   /**Ask for the node's output which ID (number) is output_id 
+      and for the 'count' iteration */
+   virtual ObjectRef getOutput(int output_id, int count)
+   {
+      if (output_id==outputID) return value;
+      else throw new NodeException (this, "DCMatrix: Unknown output id", __FILE__, __LINE__);
+   }
+
+protected:
+   /**Default constructor, should not be used*/
+   DCMatrix() {throw new GeneralException("DCMatrix copy constructor should not be called",__FILE__,__LINE__);}
+
+};
