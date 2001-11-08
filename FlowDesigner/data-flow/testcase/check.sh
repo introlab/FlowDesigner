ff9a409435dbd0c76a5ee80286c6196ee259e36b
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2001-11-08 03:42:09 +0000

    Useless but cool!

--- FlowDesigner/data-flow/testcase/check.sh
+++ FlowDesigner/data-flow/testcase/check.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+SETCOLOR_SUCCESS="echo -en \\033[1;32m"
+SETCOLOR_FAILURE="echo -en \\033[1;31m"
+SETCOLOR_WARNING="echo -en \\033[1;33m"
+SETCOLOR_NORMAL="echo -en \\033[0;39m"
+
+echo_success() {
+echo -n "[  "
+$SETCOLOR_SUCCESS
+echo -n "OK"
+$SETCOLOR_NORMAL
+echo "  ]"
+}
+
+echo_failure() {
+echo -n "[  "
+$SETCOLOR_FAILURE
+echo -n "FAILED"
+$SETCOLOR_NORMAL
+echo "  ]"
+}
+
 for file in *.n non-existant.n
 	do
 	echo -n "$file... "
@@ -7,18 +28,19 @@ for file in *.n non-existant.n
 		then
 		if batchflow $file | grep -qf $file.out
 			then
-			echo passed
+			echo_success
 		else
-			echo "** $file failed **"
+			echo -n "$file... "
+			echo_failure
 			echo
 		fi
 	else
 		batchflow $file > /dev/null 2>&1
 		if test $? -lt 2
 			then
-			echo passed
+			echo_success
 		else
-			echo "**failed **"
+			echo_failure
 			echo
 		fi
 	fi
