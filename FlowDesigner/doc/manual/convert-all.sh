3de988bd80c6eee7713ed3eded97724c2e51fac8
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2002-04-29 23:11:05 +0000

    Added two figures

--- FlowDesigner/doc/manual/convert-all.sh
+++ FlowDesigner/doc/manual/convert-all.sh
@@ -3,4 +3,6 @@ export VFLOW_PATH=
 convert vflow.png vflow.ps
 convert codegen.png codegen.ps
 convert overflow_IO.png overflow_IO.ps
+convert overflow_if.png overflow_if.ps
+convert overflow_hello.png overflow_hello.ps
 texflow | sed 's/_/\\_/g' > nodes.tex
