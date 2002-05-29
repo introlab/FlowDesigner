d444ca4d394549055a828b58ccf0b00df8b909cf
nynon <nynon@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2002-05-29 20:59:09 +0000

    More examples in user manual.

--- FlowDesigner/doc/manual/convert-all.sh
+++ FlowDesigner/doc/manual/convert-all.sh
@@ -5,4 +5,12 @@ convert codegen.png codegen.ps
 convert overflow_IO.png overflow_IO.ps
 convert overflow_if.png overflow_if.ps
 convert overflow_hello.png overflow_hello.ps
+convert overflow_min.png overflow_min.ps
+convert overflow_textProbe.png overflow_textProbe.ps
+convert overflow_matrix.png overflow_matrix.ps
+convert overflow_vector.png overflow_vector.ps
+convert overflow_main.png overflow_main.ps
+convert overflow_subnet.png overflow_subnet.ps
+convert overflow_iterator2.png overflow_iterator2.ps
+convert overflow_main3.png overflow_main3.ps
 texflow | sed 's/_/\\_/g' > nodes.tex
