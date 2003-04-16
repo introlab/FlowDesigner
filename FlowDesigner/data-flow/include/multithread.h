bc054ce4dfbf8b150695818632b4b90fad0ca8f4
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2003-04-16 21:35:07 +0000

    Added an atomic reference counter

--- FlowDesigner/data-flow/include/multithread.h
+++ FlowDesigner/data-flow/include/multithread.h
@@ -1,67 +1,71 @@
-// Copyright (C) 1999 Jean-Marc Valin
+// Copyright (C) 2003 Jean-Marc Valin
 
 #ifndef MULTITHREAD_H
 #define MULTITHREAD_H
 
 #ifdef MULTITHREAD
 
-#include "Node.h"
-#include <set>
 #include <pthread.h>
 
-class ThreadSet {
-   set<pthread_t> runningThreads;
-public:
-   void start(void * (*funct) (void *), void *args)
-   {
-      pthread_t newThread;
-      pthread_create(&newThread, NULL, funct, args);
-      runningThreads.insert(newThread);
-   }
-   void wait()
-   {
-      for (set<pthread_t>::iterator thread = runningThreads.begin() ; thread != runningThreads.end() ; thread++)
-      {
-         pthread_join(*thread,NULL);
-         runningThreads.erase(*thread);
-      }
-   }
-   ~ThreadSet() {wait();}
-};
+#if defined(_ENABLE_X86_ASM)
 
-
-
-class ThreadedGetOutputArgs {
-public:
-   ThreadedGetOutputArgs (ObjectRef *_ref, Node *_node, int _outputID, int _count) 
-      : ref(_ref) , node(_node) , outputID(_outputID) , count(_count)
-   {}
-   ObjectRef *ref;
-   Node *node;
-   int outputID;
+class AtomicCounter {
    int count;
-};
-
-
-inline void getOutputFromThread (ThreadedGetOutputArgs *args)
-{
-   //cerr << "Launching node " << args->node->getName() << " for count " << args->count << endl;
-   *(args->ref) = args->node->getOutput(args->outputID, args->count);
-   //cerr << "Ending node " << args->node->getName() << " for count " << args->count << endl;
-   delete args;
-}
-
-class NodeThreadSet : public ThreadSet
-{
 public:
-   void getOutput (ObjectRef *ref, Node *node, int outputID, int count)
+   AtomicCounter() : count(0) {}
+   void inc() 
    {
-      ThreadedGetOutputArgs *args = new ThreadedGetOutputArgs(ref, node, outputID, count);
-
-      start ((void * (*)(void *)) getOutputFromThread, args);
+      __asm__ __volatile__ ("lock; add $1, %0" : : "m" (count) : "memory");
+   }
+   bool dec()
+   {
+      int tmp;
+      __asm__ __volatile__ ("lock; sub $1, %1\n\tmov $0, %0\n\tjle out%=\n\tmov $1, %0\n\tout%=:" : "=r" (tmp): "m" (count) : "memory");
+      return tmp;
    }
 };
 
-#endif
+#else /*defined(_ENABLE_X86_ASM)*/
 
-#endif
+class AtomicCounter {
+   int count;
+   pthread_mutex_t mutex;
+public:
+   AtomicCounter() : count(0) {pthread_mutex_init(&mutex, NULL);}
+   ~AtomicCounter() {pthread_mutex_destroy(&mutex);}
+
+   void inc() 
+   {
+      pthread_mutex_lock(&mutex);
+      ++count;
+      pthread_mutex_unlock(&mutex);
+   }
+
+   bool dec() 
+   {
+      int tmp;
+      pthread_mutex_lock(&mutex);
+      --count;
+      tmp=count;
+      pthread_mutex_unlock(&mutex);
+      return (tmp > 0);
+   }
+};
+
+#endif /*defined(_ENABLE_X86_ASM)*/
+
+#else /*ifdef MULTITHREAD*/
+
+class AtomicCounter {
+   int count;
+public:
+   AtomicCounter() : count (0) {}
+   void inc() {++count;}
+   bool dec() {--count;return (count > 0);}
+};
+
+#endif /*ifdef MULTITHREAD*/
+
+
+
+#endif /*ifndef MULTITHREAD_H*/
