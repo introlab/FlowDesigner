3283b6484aaec66f87757fea1b529174d77e7d51
maestro <maestro@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2003-12-01 13:26:31 +0000

    added isNil

---
+++ FlowDesigner/data-flow/src/isNil.cc
@@ -0,0 +1,60 @@
+#ifndef _ISNIL_CC_
+#define _ISNIL_CC_
+
+#include "BufferedNode.h"
+
+
+class isNil;
+
+DECLARE_NODE(isNil)
+
+/*Node
+ * @name isNil
+ * @category RobotFlow:Generic
+ * @description No description available
+ *
+ * @input_name INPUT
+ * @input_description Float value
+ *
+ * @output_name OUTPUT
+ * @output_type bool
+ * @output_description Float transformation to integer
+ *
+END*/
+
+
+class isNil : public BufferedNode {
+   
+  //inputs
+  int inputID;
+
+  //outputs
+  int outputID;
+
+public:
+
+   isNil(string nodeName, ParameterSet params)
+   : BufferedNode(nodeName, params) {
+
+     //inputs
+     inputID = addInput("INPUT");
+     //outputs
+     outputID = addOutput("OUTPUT");
+   }
+
+   void calculate(int output_id, int count, Buffer &out) {
+
+     if (getInput(inputID,count)->isNil()) {
+       out[count] = ObjectRef(Bool::alloc(true));
+     }
+     else {
+       out[count] = ObjectRef(Bool::alloc(false));
+     }
+
+   }//calculate
+  
+};
+
+
+
+#endif
