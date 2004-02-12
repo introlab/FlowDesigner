7654f4298b54143a08394c64c0287aed5f2b44ac
jmvalin <jmvalin@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2004-02-12 16:16:59 +0000

    Forgot to add that :(

---
+++ FlowDesigner/data-flow/include/conversion.h
@@ -0,0 +1,48 @@
+// Copyright (C) 2004 Jean-Marc Valin & Dominic Letourneau
+
+#ifndef CONVERSION_H
+#define CONVERSION_H
+
+#include "ObjectRef.h"
+#include "typemap.h"
+
+
+typedef ObjectRef (*conv_func)(ObjectRef);
+
+class Conversion {
+   static TypeMap<TypeMap<conv_func> > &conv_table();
+ public:
+   template<class T, class U>
+   static int addConvFunction(conv_func func) 
+   {
+      conv_table()[&typeid(T)][&typeid(U)] = func;
+      return 0;
+   }
+   
+   template <class T>
+   static ObjectRef convertTo(ObjectRef x)
+   {
+      TypeMap<TypeMap<conv_func> >::iterator it = conv_table().find(&typeid(*x));
+      if (it != conv_table().end())
+      {
+         TypeMap<conv_func>::iterator it2 = it->second.find(&typeid(T));
+         if (it2 != it->second.end())
+         {
+            return it2->second(x);
+         } else {
+            cerr << "Cannot cast this to type requested\nThis needs to throw an exception\n";
+            return nilObject;
+         }
+      } else {
+         cerr << "Cannot cast\nThis needs to throw an exception\n";
+         return nilObject;
+      }
+   }
+};
+
+
+#define REGISTER_CONVERSION(from, to, func) \
+        static int dummy_conv_table_init_for ## _ ## from ## _ ## to =\
+        Conversion::addConvFunction<from,to>(func);
+
+#endif
