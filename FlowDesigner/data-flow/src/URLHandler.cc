db02b1d0e9a8778519122c0783b879aaeffd5e53
maestro <maestro@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2004-04-05 14:08:43 +0000

    added TCP url stream

--- FlowDesigner/data-flow/src/URLHandler.cc
+++ FlowDesigner/data-flow/src/URLHandler.cc
@@ -8,6 +8,15 @@
 #include "net_types.h"
 #include <sstream>
 #include <fstream>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include "stream_wrap.h"
 
 //@implements core
 
@@ -52,3 +61,90 @@ ObjectRef file_url_handler(const string& url, int flags) {
   return nilObject;
 }
 REGISTER_URL_HANDLER(file,file_url_handler);
+
+
+ObjectRef tcp_url_handler(const string& url, int flags) {
+
+  //FORMAT tcp:hostname:port
+
+  int _pos = url.find(":");
+
+  string stripped_url = url.substr(_pos + 1, url.size());
+
+  _pos = stripped_url.find(":");
+
+  if (_pos != string::npos) {
+
+    stripped_url[_pos] = ' ';
+    istringstream my_stream(stripped_url);
+
+    string host;
+    int port;
+
+    my_stream >> host;
+    my_stream >> port;
+
+    //Create the socket, code taken from TCPConnect
+
+    struct sockaddr_in addr;
+    int fd = socket(PF_INET, SOCK_STREAM, 0);
+    //int port = 2313;
+    struct hostent *entp;
+   
+    memset(&addr, 0, sizeof(struct sockaddr));
+   
+    addr.sin_family = AF_INET;
+      
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(0);
+    
+    if (bind (fd, (struct sockaddr *)&addr, sizeof(addr)))
+      throw new GeneralException(string("tcp_url_handler bind failed: ") + string(strerror(errno)), __FILE__, __LINE__);
+    
+    if((entp = gethostbyname(host.c_str())) == NULL)
+      throw new GeneralException(string("tcp_url_handler Can't get host by name: ") + host, __FILE__, __LINE__);
+      
+    memcpy(&addr.sin_addr, entp->h_addr_list[0], entp->h_length);
+    
+    addr.sin_port = htons(port);
+      
+    if (connect (fd, (struct sockaddr *)&addr, sizeof(addr)))
+      throw new GeneralException(string("tcp_url_handler connect failed: ") + string(strerror(errno)), __FILE__, __LINE__);
+    
+    //TODO do something for blocking / non blocking streams, include that into flags?
+
+    /*
+    if (!blocking)
+      fcntl(fd, F_SETFL, O_NONBLOCK);
+    */
+
+    //create proper type of stream
+    switch(flags) {
+      
+    case URLHandler::URL_READ:
+      return ObjectRef (new IStream (new fd_istream(fd)));
+      break;
+      
+    case URLHandler::URL_WRITE:
+      return ObjectRef (new OStream (new fd_ostream(fd)));
+      break;
+      
+    case URLHandler::URL_READWRITE:
+      return ObjectRef (new IOStream (new fd_iostream(fd)));
+      break;
+      
+    default:
+      ostringstream my_stream;
+      my_stream<<"Unknown flags "<<flags<<" in tcp_url_handler";
+      throw new GeneralException(my_stream.str(),__FILE__,__LINE__);
+      break;
+    }
+  }
+  else {
+    throw new GeneralException(string("no port specified for TCP URL : ") + stripped_url,__FILE__,__LINE__);
+  }
+  
+  //should not happen...
+  return nilObject;
+}
+REGISTER_URL_HANDLER(tcp,tcp_url_handler);
