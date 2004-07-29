3e73002eb7ddc8604c3a6afa34c8c211db612c11
carle_cote <carle_cote@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2004-07-29 18:09:58 +0000

    Add Power block

---
+++ FlowDesigner/data-flow/include/Power.h
@@ -0,0 +1,44 @@
+/* Copyright (C) 2003 Victor Bao Long Tran(v.tran@usherbrooke.ca)
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+#ifndef _POWER_H_
+#define _POWER_H_
+
+#include "BufferedNode.h"
+
+class Power : public BufferedNode
+{
+protected:
+   //inputs
+   int m_baseInID;
+   int m_expID;
+
+   //outputs
+   int m_outputID;
+
+   //parameters
+
+public:
+
+   Power(string nodeName, ParameterSet params);
+
+   virtual ~Power();
+
+   void calculate(int output_id, int count, Buffer &out);
+
+};
+
+#endif //_POWER_H_
---
+++ FlowDesigner/data-flow/src/Power.cc
@@ -0,0 +1,71 @@
+/* Copyright (C) 2003 Victor Bao Long Tran(v.tran@usherbrooke.ca)
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+#ifndef _POWER_CC_
+#define _POWER_CC_
+
+#include "Power.h"
+
+DECLARE_NODE(Power);
+
+/*Node
+ *
+ * @name Power
+ * @category Operator
+ * @description none
+ *
+ * @input_name BASE
+ * @input_type int
+ * @input_description base
+ *
+ * @input_name EXP
+ * @input_type int
+ * @input_description exponent
+ *
+ * @output_name OUTPUT
+ * @output_type int
+ * @output_description base power exponent
+ *
+END*/
+
+Power::Power(string nodeName, ParameterSet params)
+   : BufferedNode(nodeName, params)
+{
+   //inputs
+   m_baseInID = addInput("BASE");
+   m_expID = addInput("EXP");
+
+   //outputs
+   m_outputID = addOutput("OUTPUT");
+
+   //parameters
+}
+
+
+Power::~Power()
+{
+}
+
+
+void Power::calculate(int output_id, int count, Buffer &out)
+{
+   int base = dereference_cast<int>(getInput(m_baseInID,count));
+   int exp = dereference_cast<int>(getInput(m_expID,count));
+
+   (*outputs[m_outputID].buffer)[count] = ObjectRef(Int::alloc((int)(pow((double)base, (double)exp))));
+}
+#endif //_POWER_CC_
+
