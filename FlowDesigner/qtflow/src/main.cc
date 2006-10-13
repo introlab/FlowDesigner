ddb048ca1cb8818dbd5ea9090f140c0eb11dacaf
maestro <maestro@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2006-10-13 15:15:55 +0000

    first application running

--- FlowDesigner/qtflow/src/main.cc
+++ FlowDesigner/qtflow/src/main.cc
@@ -1,10 +1,30 @@
 //Copyright (C) 2006 Dominic Letourneau (Dominic.Letourneau@USherbrooke.ca) 
 
 #include "QtFlowDesigner.h"
+#include <QApplication>
+#include "path.h"
+#include "BaseException.h"
 
 using namespace FD;
 
 int main(int argc, char* argv[])
 {
-	return 0;
+
+	try 
+	{
+      		scanDL();
+   	} 
+	catch (BaseException *e)
+   	{
+      		e->print();
+      		delete e;
+      		//exit(1);
+   	}
+
+
+
+	QApplication app(argc, argv);
+	QtFlowDesigner fd;
+	fd.show();
+	return app.exec();
 }
