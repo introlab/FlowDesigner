dd7e9dd27555ccf6112944ccb9eab41bc2ffbe66
maestro <maestro@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2007-03-05 13:33:33 +0000

    gflow equivalent = qtflow, creating new executable flowdesigner. Working on installer with cpack.

---
+++ FlowDesigner/qtflow/include/QtRunContext.h
@@ -0,0 +1,10 @@
+#ifndef _QTRUNCONTEXT_H_
+#define _QTRUNCONTEXT_H_
+
+namespace FD 
+{
+
+
+} //namespace FD
+
+#endif
---
+++ FlowDesigner/qtflow/src/qtflow.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "QtRunContext.h"
+
+using namespace FD;
+using namespace std;
+
+int main (int argc, char* argv[])
+{
+	cout<<"Hello World!"<<endl;
+	return 0;
+}
