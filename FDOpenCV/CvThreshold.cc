3be32743e652db6477b11e748f14b0ff1cf4771a
jdascenzio <jdascenzio@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2007-07-11 20:05:07 +0000

    only IPL_DEPTH_8U in output

--- FDOpenCV/CvThreshold.cc
+++ FDOpenCV/CvThreshold.cc
@@ -79,12 +79,13 @@ namespace FD {
       {
          //Read the inputs
          RCPtr<CvImage> imagePtr = getInput(m_imageInID,count);
-         CvImage* image = imagePtr->gray();     
-         
+         CvImage* imageGRAY = imagePtr->gray(); 
+         CvImage* image =  new CvImage(cvCreateImage(cvSize(imagePtr->getImage()->width, imagePtr->getImage()->height)
+            , IPL_DEPTH_8U, 1));
          int status = cvGetErrMode();
          cvSetErrMode( CV_ErrModeSilent );
          __BEGIN__; 
-         OPENCV_CALL(cvThreshold(image->getImage(), image->getImage(), m_threshold, m_max_value, m_thresholeTypeMap[m_thresholdType]));         
+         OPENCV_CALL(cvThreshold(imageGRAY->getImage(), image->getImage(), m_threshold, m_max_value, m_thresholeTypeMap[m_thresholdType]));         
          __END__;
          cvSetErrMode( status );
          if( cvGetErrStatus() != CV_StsOk  )
