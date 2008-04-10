0af56014bbaf2d989a2c24e97c185af97b25e696
maestro <maestro@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2008-04-10 20:59:01 +0000

    Not yet working. Building generic get_input functions.

--- Erlang-proto1/add.erl
+++ Erlang-proto1/add.erl
@@ -15,7 +15,15 @@ add_list([H|T]) -> H + add_list(T);
 add_list([]) -> 0.
 
 make_output(Count, Name, Value) ->
-    {Count,Name,Value}.
+    {Name,Count,Value}.
+
+
+make_input(NodeName, InputName) ->
+    %We should look in a global repository the
+    %PID associated with the node, since each node have
+    %a unique PID.
+    Pid = 0,
+    {NodeName, InputName, Pid}.
 
 
 generic_prefetch(Name,Count,[H|T]) ->   
@@ -28,9 +36,79 @@ generic_prefetch(Name,Count,[H|T]) ->
  generic_prefetch(_,_,[]) -> {notFound}.   
 
 
+generic_get_input(Name,Count,Pid) ->
+    
+    %Send calculate message
+    Pid ! {self(), {calculate,Name,Count}},
+    
+    %Wait for the answer
+    %Question : Can we get other messages during this time ???
+    %We suppose that is the case...
+    receive
+        {Pid, Answer} ->
+            case Answer of
+                {ok, {Name,Count,Data} }-> [{Name,Count,Data}];
+
+                %This should throw an exception
+                {error, _} -> []
+
+            end;
+        
+        %Other messages should be put back to our message Queue?
+        Other -> self() ! Other,
+        
+        %We should wait for the answer
+        generic_get_input(Name,Count,Pid)
+        
+        
+        %Add a timer to return to main loop if
+        %taking too much time
+    end.
+    
+ generic_get_all_inputs([InputsH|InputsT]) ->   
+    %Get the input fields 
+    {Name,Count,Pid} = InputsH,
+    Result = generic_get_input(Name,Count,Pid),     
+    [Result] ++ generic_get_all_inputs(InputsT);
+ generic_get_all_inputs([]) -> [].
 	
 add_loop(Inputs, Outputs, Parameters) ->
 	receive
+        
+        %Real Calculate will fetch inputs 
+        %From another process (node)
+        {From, {calculate, OutputName, Count}} ->
+            
+            %Already calculated?
+            PreFetch = generic_prefetch("OUTPUT",Count,Outputs),
+        
+            %output Prefetch for debug
+            io:format("+++debug+++ Prefetch = ~p~n",[PreFetch]),
+            
+            case PreFetch of
+                    {notFound} -> 
+                
+                        %Get our inputs
+                        L = generic_get_all_inputs(Inputs),
+        
+                        %Store value 
+                        Result = make_output(Count,"OUTPUT",add_list(L)),
+                
+                        %Put it in the Output Buffer
+                        %Buffer should be managed according to 
+                        %N lookback / lookahead values
+                        NewOutputs = Outputs ++ [Result],
+        
+                        From ! {self(), {ok, Result} };
+                        
+                     {_,_,_} -> 
+                        NewOutputs = Outputs, 
+                        From ! {self(), {ok, PreFetch} }
+                end,
+                        
+                add_loop(Inputs,NewOutputs,Parameters);
+            
+            
 	{From, {calculate, Count, Val1, Val2} } ->
             
                 %Verify if we have already calculated this
@@ -61,19 +139,24 @@ add_loop(Inputs, Outputs, Parameters) ->
                 end,
                         
                 add_loop(Inputs,NewOutputs,Parameters);
-                
-	{From, {calculate, Count, L } } ->
-		From ! {self(), {ok, Count, add_list(L)}},
-		add_loop(Inputs,Outputs,Parameters);	
+                       
         {From, {inputs}} ->
                 From ! {self(), {inputs, Inputs}},
                 add_loop(Inputs,Outputs,Parameters);
+        
         {From, {outputs}} ->
                 From ! {self(), {outputs, Outputs}},
                 add_loop(Inputs,Outputs,Parameters);
+                
         {From, {parameters}} ->
                 From ! {self(), {parameters, Parameters}},
                 add_loop(Inputs,Outputs,Parameters); 
+                
+        {From, {connect, NodeName, OutputName, InputName,Pid}} ->
+                NewInputs = Inputs ++ [{NodeName,OutputName,InputName,Pid}],
+                From ! {self(), {connect, ok}},
+                add_loop(NewInputs,Outputs,Parameters);
+                
         {From, Other} ->
 		From ! {self(), {error, Other} } ,
 		add_loop(Inputs,Outputs,Parameters)
@@ -81,6 +164,12 @@ add_loop(Inputs, Outputs, Parameters) ->
 	
 test() ->
 	Pid = add:start([],[],[]),
+    
+        Pid2 = add:start([],[],[]),
+    
+        add:rpc(Pid, {connect,"TESTNODE","OUTPUT","INPUT",Pid2}),
+    
+    
 	add:rpc(Pid, {calculate,0,2,3}),add:rpc(Pid, {inputs}), 
             add:rpc(Pid, {outputs}), 
             add:rpc(Pid, {parameters}),
@@ -89,6 +178,7 @@ test() ->
             add:rpc(Pid, {outputs}), 
             add:rpc(Pid, {parameters}),
         
+        %Trying to get already calculated value
         add:rpc(Pid, {calculate,1,2,3}),add:rpc(Pid, {inputs}), 
             add:rpc(Pid, {outputs}), 
             add:rpc(Pid, {parameters}),
