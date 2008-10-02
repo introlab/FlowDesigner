8d5c0b456683fde51d378a02fbb4c54b5fc6af4e
maestro <maestro@aa42df38-3a4e-474a-a597-ad7e395f5a33>
2008-10-02 18:37:13 +0000

    added more tests

--- FlowDesigner/data-flow/gtest/t_UITerminal.cc
+++ FlowDesigner/data-flow/gtest/t_UITerminal.cc
@@ -1,3 +1,32 @@
 #include <limits.h>
 #include <gtest/gtest.h>
+#include "UITerminal.h"
+#include "UIDocument.h"
 
+
+TEST(UITerminal,ALLOC_TEST)
+{
+	FD::ItemInfo info;
+
+	info.name = "NONAME";
+	info.type = "Int";
+	info.value = "";
+	info.description = "Integer value";
+
+	//UITerminal (ItemInfo *terminalInfo, UINode *_node, bool _isInput, double _x, double _y);
+	FD::UITerminal *term = new FD::UITerminal(&info,NULL,true,11.0,10.0);
+
+	EXPECT_EQ(term->getName(),"NONAME");
+	EXPECT_EQ(term->getType(), "Int");
+	EXPECT_EQ(term->getDescription(),"Integer value");
+
+	EXPECT_EQ(term->getNode(), (FD::UINode*)(NULL));
+	EXPECT_EQ(term->isInputTerminal(), true);
+	EXPECT_EQ(term->isConnected(),false);
+	EXPECT_EQ(term->getNetTerminal(),(FD::UINetTerminal*)NULL);
+	EXPECT_EQ(term->getConnections().size(),0);
+
+
+	delete term;
+
+}
